bd04bfce6e0b28e160bb8bf059ec95a9a87fb19f
Minura Punchihewa <minura.jkh@keells.com>
2022-09-02 01:44:29 +0530

    added an implementation of the Factory pattern

---
+++ client.py
@@ -0,0 +1,25 @@
+from factory.ShapeFactory import ShapeFactory
+
+if __name__ == "__main__":
+    # build a square
+    square = ShapeFactory.build_shape(
+        'Square',
+        {
+            "length": 4
+        }
+    )
+
+    print(square)
+    print(square.perimeter())
+
+    # build a rectangle
+    rectangle = ShapeFactory.build_shape(
+        'Rectangle',
+        {
+            "length": 6,
+            "breadth": 5
+        }
+    )
+
+    print(rectangle)
+    print(rectangle.perimeter())
---
+++ factory/__init__.py
@@ -0,0 +1,3 @@
+from factory.IShape import IShape
+from factory.Square import Square
+from factory.Rectangle import Rectangle
