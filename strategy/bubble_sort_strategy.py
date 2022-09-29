b327a9e810ee222be929f0901921ce2c9d34fcfa
Minura Punchihewa <minura.jkh@keells.com>
2022-09-29 22:55:37 +0530

    fixed bugs in the BubbleSortStrategy

--- strategy/bubble_sort_strategy.py
+++ strategy/bubble_sort_strategy.py
@@ -3,10 +3,12 @@ from strategy import IStrategy
 
 class BubbleSortStrategy(IStrategy):
     def sort(self, inputs):
+        sorted = False
+
         while not sorted:
             sorted = True
-            for i in range(0, len(inputs)):
-                if inputs[i + 1] > inputs[i]:
+            for i in range(0, len(inputs) - 1):
+                if inputs[i + 1] < inputs[i]:
                     sorted = False
                     inputs[i], inputs[i + 1] = inputs[i + 1], inputs[i]
 
