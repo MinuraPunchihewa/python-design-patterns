96b3643aeae1840b12f4feeeec68426a15d07b8d
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-03-04 23:38:05 +0530

    updated the name of the internal list in subject

--- behavioral/observer/subject.py
+++ behavioral/observer/subject.py
@@ -3,6 +3,9 @@ from abc import ABC, abstractmethod
 
 # may also be called Publisher
 class Subject(ABC):
+    def __init__(self) -> None:
+        self._observers = []
+
     @abstractmethod
     def register(self, observer: Observer) -> None:
         pass
