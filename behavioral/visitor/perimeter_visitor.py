a1260119c4df284e9541a8fa2462de439142d0a7
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-03-23 17:32:03 +0530

    added concrete Perimeter and Area Visitor implementations

---
+++ behavioral/visitor/perimeter_visitor.py
@@ -0,0 +1,12 @@
+from visitor import IVisitor
+
+
+class PerimeterVisitor(IVisitor):
+    def visit_rectangle(self, rectangle):
+        return 2 * (rectangle.width + rectangle.height)
+
+    def visit_circle(self, circle):
+        return 2 * 3.14 * circle.radius
+
+    def visit_square(self, square):
+        return 4 * square.length
